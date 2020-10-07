d3b83dd6c09af4a5dfde6341fe0fb0cc135b5d3c
alkoxx <jonjon.a@gmail.com>
2020-10-07 23:20:19 +0200

    Creating general layout

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,70 @@
 <template>
   <v-app>
     <v-container>
-      <v-row>
-        <v-col cols="2">
-          <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
-        </v-list-item-avatar>
+      <v-app-bar
+        dense
+        dark
+      > 
+        <v-app-bar-nav-icon @click="drawer = !drawer" />
+        <v-toolbar-title>Txoko booking</v-toolbar-title>
+      </v-app-bar>
+ 
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >        
+          <v-list-group no-action>
+            <template #prependIcon>
+              <v-icon small>
+                mdi-calendar
+              </v-icon>
+            </template>
+            <template #activator>
+              <v-list-item-content>
+                <v-list-item-title>My events</v-list-item-title>              
+              </v-list-item-content>
+            </template>
 
-        <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>April 10...</v-list-item-title>       
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          
+          <v-list-group no-action>
+            <template #prependIcon>
+              <v-icon small>
+                mdi-bell-ring
+              </v-icon>
+            </template>
+            <template #activator>
+              <v-list-item-content>
+                <v-list-item-title>Notifications</v-list-item-title>              
+              </v-list-item-content>
+            </template>
 
-      <v-divider></v-divider>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>New event...</v-list-item-title>    
+                <v-list-item-subtitle>New event...</v-list-item-subtitle>       
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-navigation-drawer>
 
-      <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-        </v-col>
-        <v-col cols="10">
+      <v-row>        
+        <v-col>
           <Calendar />
         </v-col>
       </v-row>
     </v-container>
-    
   </v-app>
 </template>
 
@@ -56,8 +78,8 @@ export default {
     Calendar,
   },
 
-  data: () => ({
-    //
+  data: () => ({    
+      drawer: false,    
   }),
 };
 </script>
