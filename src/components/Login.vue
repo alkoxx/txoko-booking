5196eda60f2ad5c0a63d51763f189215b674ef66
alkoxx <jonjon.a@gmail.com>
2020-10-27 00:12:47 +0100

    Add slide effect to login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,18 +9,19 @@
       md="8"
     >
       <v-card class="elevation-12">
-        <v-window :v-model="window">
-          <v-window-item :value="1">
-            <v-row>
+        <v-window v-model="step">
+          <v-window-item>
+            <v-row class="fill-height">
               <v-col
                 cols="12"
                 md="8"
               >
                 <v-card-text class="mt-12">
-                  <h1>Sign in</h1>
+                  <h1 class="text-center display-2 light-blue--text text--darken-1">
+                    Sign in
+                  </h1>
                   <div
-                    class="text-center"
-                    mt-4
+                    class="text-center mt-4"                    
                   >
                     <v-btn
                       fab
@@ -40,7 +41,7 @@
                       name="Email"
                       prepend-icon="mdi-email"
                       type="text"
-                      color="teal accent-3"
+                      color="light-blue darken-1"
                       required
                     />
                     <v-text-field
@@ -50,7 +51,7 @@
                       prepend-icon="mdi-lock"
                       :append-icon="showPass?'mdi-eye':'mdi-eye-off'"
                       :type="showPass ? 'text' : 'password'"
-                      color="teal accent-3"
+                      color="light-blue darken-1"
                       required
                       @click:append="showPass = !showPass"
                     />
@@ -59,7 +60,7 @@
                 <div class="text-center mt-3">
                   <v-btn
                     rounded
-                    color="teal accent-3"
+                    color="light-blue darken-1"
                     dark
                     @click="login"
                   >
@@ -70,14 +71,14 @@
               <v-col
                 cols="12"
                 md="4"
-                class="teal accent-3"
+                class="light-blue darken-1"
               >
                 <v-card-text class="white--text mt-12">
                   <h1 class="text-center text-h4">
-                    Hello, Friends !
+                    Start booking !
                   </h1>
                   <h5 class="text-center">
-                    Enter your personal details
+                    If you are new enter your personal details and create an account
                   </h5>
                 </v-card-text>
                 <div class="text-center mt-3">
@@ -85,7 +86,7 @@
                     rounded
                     outlined                    
                     dark
-                    @click="window++"
+                    @click="step++"
                   >
                     SIGN UP
                   </v-btn>
@@ -93,23 +94,104 @@
               </v-col>
             </v-row>
           </v-window-item>
-          <v-window-item :value="2" />
+          <v-window-item>
+            <v-row class="fill-height">
+              <v-col
+                cols="12"
+                md="4"
+                class="light-blue darken-1"
+              >
+                <v-card-text class="white--text mt-12">
+                  <h1 class="text-center text-h4">
+                    Welcome back !
+                  </h1>
+                  <h5 class="text-center">
+                    If you already have an account login with your credentials
+                  </h5>
+                </v-card-text>
+                <div class="text-center mt-3">
+                  <v-btn
+                    rounded
+                    outlined                    
+                    dark
+                    @click="step--"
+                  >
+                    SIGN IN
+                  </v-btn>
+                </div>
+              </v-col>
+              <v-col
+                cols="12"
+                md="8"
+              >
+                <v-card-text class="mt-12">
+                  <h1
+                    class="text-center display-2 light-blue--text text--darken-1"
+                  >
+                    Create account
+                  </h1>
+                  <div
+                    class="text-center mt-4"                    
+                  >
+                    <v-btn
+                      fab
+                      color="black"
+                      outlined
+                      @click="socialLogin"
+                    >
+                      <v-icon>
+                        mdi-google
+                      </v-icon>
+                    </v-btn>
+                  </div>
+                  <v-form>
+                    <v-text-field
+                      v-model="name"
+                      label="Name"
+                      name="Name"
+                      prepend-icon="mdi-human"
+                      type="text"
+                      color="light-blue darken-1"
+                      required
+                    />
+                    <v-text-field
+                      v-model="email"
+                      label="Email"
+                      name="Email"
+                      prepend-icon="mdi-email"
+                      type="text"
+                      color="light-blue darken-1"
+                      required
+                    />
+                    <v-text-field
+                      v-model="password"
+                      label="Password"
+                      name="Password"
+                      prepend-icon="mdi-lock"
+                      :append-icon="showPass?'mdi-eye':'mdi-eye-off'"
+                      :type="showPass ? 'text' : 'password'"
+                      color="light-blue darken-1"
+                      required
+                      @click:append="showPass = !showPass"
+                    />
+                  </v-form>
+                </v-card-text>
+                <div class="text-center mt-3">
+                  <v-btn
+                    rounded
+                    color="light-blue darken-1"
+                    dark
+                    @click="login"
+                  >
+                    SIGN UP
+                  </v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-window-item>
         </v-window>
       </v-card>
     </v-col>
-    <v-card-text>
-      <v-row>
-        <v-col
-          class="d-flex justify-center"            
-        >            
-          <p>
-            <router-link to="/sign-up">
-              Create account
-            </router-link>
-          </p>
-        </v-col>
-      </v-row>
-    </v-card-text>
   </v-row>
 </template>
 
@@ -120,12 +202,13 @@ export default {
     name: 'Login',
     data() {
         return {
+            name: '',
             email: '',
             password: '',            
             error: null,
             showPass: false,
-            window: 1
-        }
+            step: 0
+            }
     },
     methods: {
       login(){
---
+++ src/components/window.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card
+    flat
+    tile
+  >
+    <v-window
+      v-model="onboarding"
+    >
+      <v-window-item
+        v-for="n in length"
+        :key="`card-${n}`"
+      >
+        <v-card
+          color="grey"
+          height="200"
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <h1
+              style="font-size: 5rem;"
+              class="white--text"
+            >
+              Slide {{ n }}
+            </h1>
+          </v-row>
+        </v-card>
+      </v-window-item>
+    </v-window>
+
+    <v-card-actions class="justify-space-between">
+      <v-btn
+        text
+        @click="onboarding--"
+      >
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      
+      <v-btn
+        text
+        @click="onboarding+2"
+      >
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      length: 5,
+      onboarding: 1,
+    }),
+
+    methods: {
+      
+    },
+  }
+</script>
